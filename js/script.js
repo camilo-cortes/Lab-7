852b710c506179bfca3ee204123b4555cfc30d82
Camilo Cortes <ccortes2@hawk.iit.edu>
2019-04-17 09:48:27 -0500

    changed names

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+document.getElementById("entrybutton").addEventListener("click", myFunction);
+}
 
-
-
-
-
-
+function myFunction() {
+	alert("Camilo Cortes: " + document.getElementById("entryinput").value);
+	document.getElementById("textoutput").innerHTML = document.getElementById("entryinput").value;
+}
 
 window.addEventListener('load', init);
